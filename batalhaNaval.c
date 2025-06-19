fa7d1d306783d616047a6484344a5d900c477a7b
Ursula Diefenbach <diefenbach.ushy@gmail.com>
2025-06-19 03:07:21 +0000

    Desafio Batalha Naval - Nível Aventureiro concluído

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -2,24 +2,44 @@
 
 // Desafio Batalha Naval - MateCheck
 
+// ** Definindo constantes para o tamanho do tabuleiro, tamanho do navio e valores representativos
+#define LINHAS 10
+#define COLUNAS 10
+#define TAMANHO_TABULEIRO 10 // Tamanho do tabuleiro, que é 10x10
+#define TAMANHO_NAVIO 3 // Tamanho do navio, que é 3 para este desafio
+#define VALOR_NAVIO 3 // Valor que representa uma parte do navio no tabuleiro
+
+// Função recursiva para inicialização dos navios
+void inicializarNavio(int vetor[], int indice) {
+    if(indice >= TAMANHO_NAVIO) return; // Condição de parada: se o índice for igual ao tamanho do navio, retorna
+   
+
+    vetor[indice] = VALOR_NAVIO; // Atribui o valor representativo do navio
+    inicializarNavio(vetor, indice + 1);
+}
+
+
 int main() {
-    // **  Nível Novato - Posicionamento dos Navios
 
-    // ** Declarando as variáveis necessárias para o jogo.
+    // ** Declarando as variáveis  necessárias para o jogo.
 
-    char coluna[10]; // Colunas do tabuleiro
-    int linha[10]; // Linhas do tabuleiro, de 1 a 10
+    char coluna[COLUNAS]; // Colunas do cabeçalho do tabuleiro
+    int linha[LINHAS]; // Linhas do tabuleiro, de 1 a 10
 
-    int tabuleiro[10][10]; // Declaração da  matriz bidimensional para representar o tabuleiro.
-    int tamanho_tabuleiro = 10; // Armazena o tamanho do tabuleiro, 10x10
+    int tabuleiro[LINHAS][COLUNAS]; // Declaração da  matriz bidimensional para representar o tabuleiro.
     
-    int navio1[3]; // Declarando o vetor do navio 1 tamanho 3 - representando as partes do navio
-    int posicao_navio1_i = 2, posicao_navio1_j = 5; // Variável para armazenar as coordenadas do navio 1 (j para coluna, i para linha)
-    int tamanho_navio1 = 3; // Variável para armazenar o tamanho do navio 1 (3 partes)
-
-    int navio2 [3]; // Declarando o vetor do navio 2 tamanho 3 - representando as partes do navio
-    int posicao_navio2_i = 3 , posicao_navio2_j = 2; // Variável para armazenar as coordenadas do navio 2 (j para coluna, i para linha)
-    int tamanho_navio2 = 3; // Variável para armazenar o tamanho do navio 2 (3 partes)
+    int navio1[TAMANHO_NAVIO]; // Declarando o vetor do navio 1 tamanho 3 - representando as partes do navio
+    int posicao_navio1_i, posicao_navio1_j; // Variável para armazenar as coordenadas do navio 1 (j para coluna, i para linha)
+    
+    int navio2[TAMANHO_NAVIO]; // Declarando o vetor do navio 2 tamanho 3 - representando as partes do navio
+    int posicao_navio2_i, posicao_navio2_j; // Variável para armazenar as coordenadas do navio 2 (j para coluna, i para linha)
+    
+    
+    int navio3[TAMANHO_NAVIO]; // Declarando o vetor do navio 3 tamanho 3 - representando as partes do navio
+    int posicao_navio3_i, posicao_navio3_j; // Variável para armazenar as coordenadas do navio 3 (j para coluna, i para linha)
+    
+    int navio4[TAMANHO_NAVIO]; // Declarando o vetor do navio 4 tamanho 3 - representando as partes do navio
+    int posicao_navio4_i, posicao_navio4_j; // Variável para armazenar as coordenadas do navio 4 (j para coluna, i para linha)
 
     // Declarando variavel de controle para permissão de posicionamento - valor inicial de 1 (pode ocupar posição)
     int posicao_permitida = 1; // Variável para controlar se a posição é permitida para o navio
@@ -49,27 +69,30 @@ int main() {
     }
 
     // Inicializando o navio 1
+    inicializarNavio(navio1, 0); // Chama a função recursiva para inicializar o navio 1
 
-    for (int i = 0; i < 3; i++) //Percorre as linhas do vetor
-    {
-        navio1[i] = 3; // Inicializa o navio com 3 representando parte do navio
-    }
-        
     // Inicializando o navio 2
-
-    for (int i = 0; i < 3; i++) //Percorre as linhas do vetor
-    {
-        navio2[i] = 3; // Inicializa o navio com 3 representando parte do navio
-    }
-
-
+    inicializarNavio(navio2, 0); // Chama a função recursiva para inicializar o navio 2
+    
+    // Inicializando o navio 3
+    inicializarNavio(navio3, 0); // Chama a função recursiva para inicializar o navio 3
+    
+    // Inicializando o navio 4
+    inicializarNavio(navio4, 0); // Chama a função recursiva para inicializar o navio 4
+          
     // ** Verificando se navio 1 está dentro dos limites do tabuleiro - Horizontal
-    if (posicao_navio1_j + tamanho_navio1 > tamanho_tabuleiro)
+   
+    posicao_navio1_i = 9; // Linha 10 (índice 9) do tabuleiro
+    posicao_navio1_j = 0; // Coluna A (índice 0) do tabuleiro
+
+    if (posicao_navio1_j + TAMANHO_NAVIO > TAMANHO_TABULEIRO || posicao_navio1_i >= TAMANHO_TABULEIRO)
     {
         posicao_permitida = 0; // Se a posição do navio 1 ultrapassar o tamanho do tabuleiro, não é permitido posicionar
-    } else {
+
+    } else 
+    {
         // Verifica se já existe um navio na posição
-        for (int i = 0; i < tamanho_navio1; i++) {
+        for (int i = 0; i < TAMANHO_NAVIO; i++) {
             if (tabuleiro[posicao_navio1_i][posicao_navio1_j + i] != 0) {
                 posicao_permitida = 0; // Se já existe um navio na posição, não é permitido posicionar
                 break;
@@ -78,25 +101,31 @@ int main() {
     }
     
     // ** Inserindo o navio 1 no tabuleiro se a posição for permitida - Horizontal
-    // Definindo a posição do navio 1
+    // Definindo a posição do navio 1 se for permitida
     if (posicao_permitida) {
-        for (int i = 0; i < tamanho_navio1; i++) {
-            tabuleiro[posicao_navio1_i][posicao_navio1_j + i] = navio1[i]; // Insere o navio 1 no tabuleiro
+        for (int i = 0; i < TAMANHO_NAVIO; i++) {
+            tabuleiro[posicao_navio1_i][posicao_navio1_j + i] = VALOR_NAVIO; // Insere o navio 1 no tabuleiro com o valor da constante
             // printf("Navio 1 posicionado em: %c%d\n", coluna[posicao_navio1_j + i], linha[posicao_navio1_i]); // Exibe a posição do navio
         }
-    } else {
+    } else 
+    {
         printf("Posição inválida para o Navio 1!\n"); // Mensagem de erro se a posição não for permitida
     }
     
-
-
     // ** Verificando se navio 2 está dentro dos limites do tabuleiro - Vertical
-    if (posicao_navio2_i + tamanho_navio2 > tamanho_tabuleiro)
+    // Definindo a posição do navio 2 (linha 1, coluna E)
+    posicao_navio2_i = 0;
+    posicao_navio2_j = 4;
+    posicao_permitida = 1; // Reseta a variável de permissão para o navio 2
+
+    if (posicao_navio2_i + TAMANHO_NAVIO > TAMANHO_TABULEIRO || posicao_navio2_j >= TAMANHO_TABULEIRO)
     {
         posicao_permitida = 0; // Se a posição do navio 2 ultrapassar o tamanho do tabuleiro, não é permitido posicionar
-    } else {
+
+    } else 
+    {
         // Verifica se já existe um navio na posição
-        for (int i = 0; i < tamanho_navio2; i++) {
+        for (int i = 0; i < TAMANHO_NAVIO; i++) {
             if (tabuleiro[posicao_navio2_i + i][posicao_navio2_j] != 0) {
                 posicao_permitida = 0; // Se já existe um navio na posição, não é permitido posicionar
                 break;
@@ -105,22 +134,93 @@ int main() {
     }
     
     // ** Inserindo o navio 2 no tabuleiro se a posição for permitida - Vertical
-    // Definindo a posição do navio 2
+    // Definindo a posição do navio 2 se permitida
     if (posicao_permitida) {
-        for (int i = 0; i < tamanho_navio2; i++) {
-            tabuleiro[posicao_navio2_i + i][posicao_navio2_j] = navio2[i]; // Insere o navio 2 no tabuleiro
-            // printf("Navio 2 posicionado em: %c%d\n", coluna[posicao_navio2_j], linha[posicao_navio2_i + i]); // Exibe a posição do navio
+        for (int i = 0; i < TAMANHO_NAVIO; i++) {
+            tabuleiro[posicao_navio2_i + i][posicao_navio2_j] = VALOR_NAVIO; // Insere o navio 2 no tabuleiro com o valor da constante
+           
         }
-    } else {
+    } else 
+    {
         printf("Posição inválida para o Navio 2!\n"); // Mensagem de erro se a posição não for permitida
     }
 
+    // ** Verificando se navio 3 está dentro dos limites do tabuleiro - Diagonal principal
+    // Definindo a posição do navio 3 (linha 1, coluna G)
+    posicao_navio3_i = 0;
+    posicao_navio3_j = 6;
+    posicao_permitida = 1; // Reseta a variável de permissão para o navio 3
 
+    if (posicao_navio3_i + TAMANHO_NAVIO > TAMANHO_TABULEIRO || posicao_navio3_j + TAMANHO_NAVIO > TAMANHO_TABULEIRO)
+    {
+        posicao_permitida = 0; // Se a posição do navio 3 ultrapassar o tamanho do tabuleiro, não é permitido posicionar
+
+    } else 
+    {
+        // Verifica se já existe um navio na posição
+        for (int i = 0; i < TAMANHO_NAVIO; i++) {
+            if (tabuleiro[posicao_navio3_i + i][posicao_navio3_j + i] != 0) {
+                posicao_permitida = 0; // Se já existe um navio na posição, não é permitido posicionar
+                printf("Posição inválida para o Navio 3!\n"); // Mensagem de erro se a posição não for permitida
+                break;
+            }
+        }
+    }
+
+    // ** Inserindo o navio 3 no tabuleiro se a posição for permitida - Diagonal principal
+
+    // Definindo a posição do navio 3
+    if (posicao_permitida) {
+       for (int i = posicao_navio3_i; i < posicao_navio3_i + TAMANHO_NAVIO; i++)
+       {
+            for (int j = posicao_navio3_j; j < posicao_navio3_j + TAMANHO_NAVIO; j++)
+            {
+                if (i - posicao_navio3_i == j - posicao_navio3_j) { // Verifica se é a diagonal principal
+                    tabuleiro[posicao_navio3_i + i][posicao_navio3_j + i] = VALOR_NAVIO; // Insere o navio 3 no tabuleiro com o valor da constante
+                }
+            }
+        }
+    }
+
+    // ** Verificando se navio 4 está dentro dos limites do tabuleiro - Diagonal secundária
+    // Definindo a posição do navio 4 (linha 5, coluna E)
+    posicao_navio4_i = 4; // Linha 5 corresponde ao indice 3 
+    posicao_navio4_j = 3; // Coluna E corresponde ao indice 4
+    posicao_permitida = 1; // Reseta a variavel de permissão para o navio 4
+
+    //  verifica se ultrapassa os limites do tabuleiro e se a posição já está ocupada
+    if (posicao_navio4_i + TAMANHO_NAVIO > TAMANHO_TABULEIRO || posicao_navio4_j - TAMANHO_NAVIO + 1 < 0)
+    {
+        posicao_permitida = 0;
+    } else
+    {
+        for (int i = 0; i < TAMANHO_NAVIO; i++) {
+            if (tabuleiro[posicao_navio4_i + i][posicao_navio4_j - i] != 0) {
+                posicao_permitida = 0;
+                break;
+            }
+        }
+    }
+    
+    // ** inserindo o navio $ no tabuleiro se a posição for perrmitida - Diagonal secundário
+    if (posicao_permitida)
+    {
+        for (int i = 0; i < TAMANHO_NAVIO; i++) {
+            for (int j = 0; j < TAMANHO_NAVIO; j++) {
+                if (i == j) {
+                    tabuleiro[posicao_navio4_i + i][posicao_navio4_j - j] = VALOR_NAVIO;
+                }
+            }
+        }
+    }
+    
+    
+        
     // ** Exibindo o tabuleiro e os navios no console
     printf("** TABULEIRO BATALHA NAVAL **\n");
 
     printf("\n"); // Pula linha em branco para melhor visualização
-    printf("   "); // Espaço para as colunas
+    printf("   "); // Espaço para as colunas a insercão do cabeçalho
     
     // Loop para a impressão das colunas do tabuleiro
     for (int j = 0; j < 10; j++) {
@@ -151,30 +251,6 @@ int main() {
     printf("\n \n"); // Pula 2 linhas após imprimir o tabuleiro
      
         
-        
-        
-       
-
-
-
-    
-
-    
-    
-
-    
-
-    
-
-
-
-       
-    
-    // Sugestão: Posicione o navio 1 no tabuleiro, por exemplo, nas coordenadas (0, 0), (0, 1), (0, 2).
-    
-    
-
-    
     // TODO Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // TODO Utilize `printf` para exibir as coordenadas de cada parte dos navios.
 
@@ -206,3 +282,34 @@ int main() {
 
     return 0;
 }
+        
+       
+    
+            
+           
+           
+    
+
+        
+       
+
+
+
+    
+
+    
+    
+
+    
+
+    
+
+
+
+       
+    
+    
+    
+    
+
+    
